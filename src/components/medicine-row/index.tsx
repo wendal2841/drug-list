6dba70dac20d5e19ae7cff0f99a42b5fd4aa7159
Alex <alexander@y-tree.co>
2019-10-28 23:54:19 +0200

    fix-style-medicine-row

--- src/components/medicine-row/index.tsx
+++ src/components/medicine-row/index.tsx
@@ -12,24 +12,28 @@ export const MedicineRow: React.FC<IPropsMedicineRow> = ({
     const onDelete = (): void => deleteMedicine(id || '');
 
     return (
-        <div className="medicine-row" >
-            <span className="medicine-row__code" >Code: { code }</span>
-            <span className="medicine-row__name" >Name: { name }</span>
-            <span className="medicine-row__price" >Price: { price }</span>
-            <Button
-                className="medicine-row__edit"
-                type="primary"
-                onClick={onEdit}
-            >
-                Edit
-            </Button>
-            <Button
-                className="medicine-row__delete"
-                type="danger"
-                onClick={onDelete}
-            >
-                Delete
-            </Button>
+        <div className="medicine-row" onDoubleClick={onEdit} >
+            <div className="medicine-row__content" >
+                <span className="medicine-row__code" >Code: { code }</span>
+                <span className="medicine-row__name" >Name: { name }</span>
+                <span className="medicine-row__price" >Price: { price }</span>
+            </div>
+            <div className="medicine-row__buttons">
+                <Button
+                    className="medicine-row__edit"
+                    type="primary"
+                    onClick={onEdit}
+                >
+                    Edit
+                </Button>
+                <Button
+                    className="medicine-row__delete"
+                    type="danger"
+                    onClick={onDelete}
+                >
+                    Delete
+                </Button>
+            </div>
         </div>
     );
 };
